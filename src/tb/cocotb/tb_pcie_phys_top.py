c27a2165c75347aaf9cb16bc2e91c3e9640dcbc4
Winston <winston.ong92@gmail.com>
2025-09-02 12:05:25 -0400

    Initial cocotb commit

---
+++ src/tb/cocotb/tb_pcie_phys_top.py
@@ -0,0 +1,42 @@
+
+import random
+import math
+import cocotb
+import numpy
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, FallingEdge, Join, ClockCycles, Timer
+from cocotb.types import LogicArray
+
+# from helper_functions import get_elements, wait_n_clock_cycles, init_test, end_test
+
+class PCIe:
+    def __init__(self, dut):
+        self.dut = dut
+        clock = Clock(dut.clk_i, 10, units="ns")  # Create a 10us period clock on port clk
+        cocotb.start_soon(clock.start(start_high=False))
+
+
+    async def reset(self):
+        self.dut.rst_i.value = 0
+        await Timer(20, units='ns')
+        self.dut.rst_i.value = 1
+        await Timer(20, units='ns')
+
+
+@cocotb.test(timeout_time=10, timeout_unit="us")
+async def test_pcie_phys_top(dut):
+    '''
+    Test of PCIe Top Module
+    '''
+    test_pass = 1
+    # Initialize the test
+    pcie_inst = PCIe(dut)
+    await ClockCycles(dut.clk_i,1000)
+
+    # Write test here
+
+    # End of Test code
+    # await end_test(dut,test_pass)
+
+
+#-------------------------------------------------------------------------------------------
