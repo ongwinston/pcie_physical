43aef239c49bfb4ff2eb521d3b98a7742f1f2a76
Winston <winston.ong92@gmail.com>
2025-10-08 16:47:05 -0400

    Cocotb simulation of serializer and fix name typo

--- src/rtl/serializer/serializer.sv
+++ src/rtl/serializer/serializer.sv
@@ -13,16 +13,26 @@ module serializer #(
   // symbol_data_in
   input logic [DATA_WIDTH -1:0] symbol_data_i,
 
+  input logic  symbol_valid_i,
+
   // Analog TX
-  input logic analag_tx_clk_i,
+  input logic analog_tx_clk_i,
   output logic symbol_bit_o,
   output logic symbol_bit_valid_o
 
 );
 
-  logic [DATA_WIDTH-1 : 0]pop_data;
+  localparam COUNTER_WIDTH = $clog2(DATA_WIDTH);
+
+  logic [DATA_WIDTH-1 : 0] pop_data;
   logic fifo_full;
   logic fifo_empty;
+  logic pop_fifo;
+
+  logic [DATA_WIDTH-1 : 0] staged_data_d, staged_data_q;
+  logic [COUNTER_WIDTH-1 : 0] staging_cntr_d, staging_cntr_q;
+  logic                    serial_bit_out;
+
 
   //------------------------------------------------
   // FIFO buffer
@@ -33,20 +43,45 @@ module serializer #(
     .RESET_ACTIVE_LOW(1'b0)
   ) fifo_inst(
     .wclk         (clk_i),
-    .rclk         (analag_tx_clk_i),
+    .rclk         (analog_tx_clk_i),
     .reset        (rst_i),
-    .push_i       (1'b0),
-    .push_data_i  (DATA_WIDTH'('d0)),
-    .pop_i        (1'b0),
+    .push_i       (symbol_valid_i),
+    .push_data_i  (symbol_data_i),
+    .pop_i        (pop_fifo),
     .pop_data_o   (pop_data),
     .fifo_full_o  (fifo_full),
     .fifo_empty_o (fifo_empty)
 
   );
 
-
   //================================================
   // Serializer out on analog clock domain
   //================================================
+
+  assign staged_data_d = (pop_fifo) ? pop_data : staged_data_q >> 1'b1;
+  assign staging_cntr_d = (staging_cntr_q == (COUNTER_WIDTH)'(DATA_WIDTH-1)) ? 'd0 : staging_cntr_q + 1'b1;
+
+  // Pop fifo when not empty and our staging_cntr_q hits the data width
+  assign pop_fifo = (!fifo_empty && (staging_cntr_q == (COUNTER_WIDTH)'(DATA_WIDTH-1))) ? 1'b1 : 1'b0;
+
+  always_ff @( posedge analog_tx_clk_i ) begin
+    if(rst_i) begin
+      staged_data_q <= 'd0;
+      staging_cntr_q <= 'd0;
+    end else begin
+      staged_data_q <= staged_data_d;
+      staging_cntr_q <= staging_cntr_d;
+    end
+  end
+
+  assign serial_bit_out = staged_data_q[0];
+
+
+
+  //================================================
+  // Output assigns
+  //================================================
+  assign symbol_bit_o = serial_bit_out;
+  assign symbol_bit_valid_o = 1'b1; // TODO
 
 endmodule
--- src/rtl/serializer/tb_serializer.py
+++ src/rtl/serializer/tb_serializer.py
@@ -8,6 +8,12 @@ from cocotb.triggers import RisingEdge, FallingEdge, Join, ClockCycles, Timer
 from cocotb.types import LogicArray
 from cocotb.queue import Queue
 
+async def reverse_bits(bits):
+    '''
+    Takes in symbol of bits and reverses their order
+    '''
+    reverse_bits = bits[::-1]
+    return reverse_bits
 
 class Serializer:
     def __init__(self,dut):
@@ -22,8 +28,46 @@ class Serializer:
         assert (test_pass), f"{error_str}"
 
     async def reset(self):
+        self.dut.rst_i.value = 1
+        await RisingEdge(self.dut.clk_i)
         self.dut.rst_i.value = 0
         await RisingEdge(self.dut.clk_i)
+
+    async def push_data(self, data):
+        self.dut.symbol_data_i.value = data
+        self.dut.symbol_valid_i.value = 1
+        await RisingEdge(self.dut.clk_i)
+        self.dut.symbol_data_i.value = 0
+        self.dut.symbol_valid_i.value = 0
+
+    async def push_skip_symbol(self):
+        '''
+        K28.0
+        | HGF_EDCBA | RD- abcdei_fghj | RD+ abcdei_fghj |
+        ----------------------------------------------
+        | 000_11100 | 00_1111_0100    | 11_0000_1011    |
+
+        re-order this to 
+        jhgfiedcba
+        to push LSB bit first
+        '''
+        symbol = "0011110100"
+        symbol_reverse = await reverse_bits(symbol)
+
+        # Check Running Disparity
+        await self.push_data(int(symbol_reverse,2)) # RD-
+
+    async def push_idle_symbol(self):
+        '''K28.3'''
+        symbol = "0011110011"
+        symbol_reverse = await reverse_bits(symbol)
+        await self.push_data(int(symbol_reverse,2)) # RD-
+
+    async def push_com_symbol(self):
+        ''' K28.5'''
+        symbol = "0011111010"
+        symbol_reverse = await reverse_bits(symbol)
+        await self.push_data(int(symbol_reverse,2)) # RD-
 
 
 @cocotb.test(timeout_time=10, timeout_unit="us")
@@ -34,7 +78,7 @@ async def test_simple(dut):
 
     # Clock period 10ns = 100Mhz
     clock = Clock(dut.clk_i, 10, units="ns")
-    tx_clk = Clock(dut.analag_tx_clk_i, 5, units="ns")
+    tx_clk = Clock(dut.analog_tx_clk_i, 1, units="ns")
     cocotb.start_soon(clock.start())
     cocotb.start_soon(tx_clk.start())
 
@@ -43,6 +87,24 @@ async def test_simple(dut):
 
     await serializer.reset()
 
-    await ClockCycles(dut.clk_i,50)
+    await ClockCycles(dut.clk_i,5)
 
-    await serializer.end_test(0)
+
+    # Push data into the serializer
+    await serializer.push_data(0b1001110100) # D0.0
+    # await RisingEdge(dut.clk_i)
+    await serializer.push_data(0b0111010100) # D1.0
+    # await RisingEdge(dut.clk_i)
+    await serializer.push_data(0b1011010100) # D2.0
+    # await RisingEdge(dut.clk_i)
+    await serializer.push_data(0b1100011011) # D3.0
+    # await RisingEdge(dut.clk_i)
+
+    await serializer.push_skip_symbol()
+    await serializer.push_idle_symbol()
+    await serializer.push_com_symbol()
+
+
+    await ClockCycles(dut.clk_i, 100) # Flush out the encoded data, 10 cycles + 2
+
+    await serializer.end_test(1)
