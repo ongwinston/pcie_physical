d832dda3f7ec8ee1c495ba455f64ecbd2e3dd7d6
Winston <winston.ong92@gmail.com>
2025-10-15 15:02:31 -0400

    Move cocotb top to its own folder. And created a cocotb test environment for multilane_controller

---
+++ src/tb/cocotb/multilane_controller/tb_multilane_controller.py
@@ -0,0 +1,82 @@
+
+import random
+import math
+import cocotb
+import numpy
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, FallingEdge, ClockCycles, Timer
+from cocotb.types import LogicArray
+from cocotb.task import Join
+
+# from helper_functions import get_elements, wait_n_clock_cycles, init_test, end_test
+
+class MultilaneController:
+    def __init__(self, dut):
+        self.dut = dut
+        clock = Clock(dut.clk_i, 10, unit="ns")  # Create a 10us period clock on port clk
+        cocotb.start_soon(clock.start(start_high=False))
+
+        pll_tx_clk = Clock(dut.tx_analog_clk_i, 1, unit="ns")
+        cocotb.start_soon(pll_tx_clk.start(start_high=False))
+
+        # Define number of lanes supported
+
+
+    async def reset(self):
+        self.dut.rst_i.value = 0
+        await Timer(20, unit='ns')
+        self.dut.rst_i.value = 1
+        await Timer(20, unit='ns')
+
+    async def lane_enable(self):
+        self.dut.lane_enable_i.value = 1
+    
+    async def push_data(self, data):
+        self.dut.data_frame_i.value = data
+        self.dut.data_frame_valid_i.value = 1
+        await RisingEdge(self.dut.clk_i)
+        self.dut.data_frame_i.value = 0
+        self.dut.data_frame_valid_i.value = 0
+
+    async def disable_scrambler(self):
+        self.dut.bypass_scrambler_i.value = 1
+        await RisingEdge(self.dut.clk_i)
+    
+    async def is_ordered_set(self):
+        self.dut.is_ordered_set_i.value = 1
+        await RisingEdge(self.dut.clk_i)
+
+
+
+@cocotb.test(timeout_time=1, timeout_unit="ms")
+async def test_multilane_controller_simple(dut):
+    '''
+    Test of PCIe Multilane controller
+    '''
+    test_pass = 1
+    # Initialize the test
+    multilane_inst = MultilaneController(dut)
+
+
+    await ClockCycles(dut.clk_i,100)
+
+
+    # Write test here
+    await multilane_inst.disable_scrambler()
+
+    await ClockCycles(dut.clk_i,5)
+
+    # Push data
+    await multilane_inst.push_data(0xf)
+
+    await ClockCycles(dut.clk_i, 20)
+
+
+
+    await ClockCycles(dut.clk_i,10000)
+ 
+    # End of Test code
+    # await end_test(dut,test_pass)
+
+
+#-------------------------------------------------------------------------------------------
